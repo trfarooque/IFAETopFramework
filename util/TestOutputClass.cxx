f5bd31f4a103476262a2d8851fd29630cb9c385b
Loic Valery <lvalery@cern.ch>
2015-05-15 19:18:54 +0000

    More information in the test - Seems complete for the histograms - Before testing the trees
    git-svn-id: svn+ssh://svn.cern.ch/reps/IfaeAnaRepo/IFAEanalysis/RunIITopTools/IFAETopFramework/trunk@10509 4525493e-7705-40b1-a816-d608a930855b

--- util/TestOutputClass.cxx
+++ util/TestOutputClass.cxx
@@ -10,7 +10,7 @@ int main() {
     
     //
     //
-    // Creating ummy variables to illustrate
+    // Creating dummy variables to illustrate
     //
     //
     int Njets;
@@ -46,13 +46,17 @@ int main() {
     outputMngrHist -> setData(data);//Associates the "data" object to the OutputManager one
     
     //
-    // Creating the output histograms
+    // Creating the output standard histograms and steps
     //
     outputMngrHist->addStandardTH1( "jets_n", 1, -0.5, 15.5, "Number of jets", "I", false, &Njets);
+    outputMngrHist->addStandardTH1( "jet1_pt", 25, 0, 1000, "leading jet pt", "VF", false, &jets_pt,0);
+    outputMngrHist->addStandardTH1( "jet2_pt", 25, 0, 1000, "leading jet pt", "VF", false, &jets_pt,1);
+    outputMngrHist->addStandardTH1( "jet3_pt", 25, 0, 1000, "leading jet pt", "VF", false, &jets_pt,2);
+    outputMngrHist->addStandardTH1( "jet1_mv1", 0.05, -2, 2, "MV1", "VD", false, &jets_MV1,0);
     outputMngrHist->bookStandardTH1( "c0", false);
     outputMngrHist->bookStandardTH1( "c1", false);
     
-    const unsigned int nentries = 100;
+    const unsigned int nentries = 1000;
     TRandom3 *rdm = new TRandom3();
     
     //
@@ -61,14 +65,39 @@ int main() {
     //
     //
     for ( unsigned int iEntry = 0; iEntry < nentries; ++iEntry){
-        Njets = rdm->Poisson(5);
-        outputMngrHist->fillStandardTH1("c0");
         
+        //
+        // Filling the variables
+        //
+        jets_pt.clear();
+        jets_isBtagged.clear();
+        jets_MV1.clear();
+        jets_truthMatched_index.clear();
+        
+        Njets = rdm->Poisson(5);
+        for(unsigned int iJet = 0; iJet < Njets; ++iJet){
+            jets_pt.push_back(rdm->Gaus(400.,100)/(iJet+1));
+            double MV1 = rdm->Uniform(0,1);
+            jets_MV1.push_back(MV1);
+            if(MV1>0.7) jets_isBtagged.push_back(1);
+            else jets_isBtagged.push_back(0);
+            
+            int parton_matched = rdm->Poisson(1);
+            std::vector < int > matched;
+            for (unsigned int iMatch = 0; iMatch<parton_matched; ++iMatch) {
+                matched.push_back((int)rdm->Uniform(0,100));
+            }
+            jets_truthMatched_index.push_back(matched);
+        }
+        
+        //
+        // Filling the output
+        //
+        outputMngrHist->fillStandardTH1("c0");
         if(Njets>=6){
             outputMngrHist->fillStandardTH1("c1");
         }
     }
     outputMngrHist->saveStandardTH1("myNiceTest.root");
-    
     return 0;
 }
