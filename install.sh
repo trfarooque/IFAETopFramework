101ec5af9e042973f07a05c7ae9ebc992f530ed9
Loic Valery <lvalery@cern.ch>
2015-07-05 14:45:38 +0000

    Adding the local installation script
    git-svn-id: svn+ssh://svn.cern.ch/reps/IfaeAnaRepo/IFAEanalysis/RunIITopTools/IFAETopFramework/devbranches/dev_lvalery@10660 4525493e-7705-40b1-a816-d608a930855b

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+red=$'\033[41;1;37m'
+blue=$'\033[44;1;37m'
+green=$'\033[42;1;37m'
+yellow=$'\e[43;1;30m'
+normal=$'\033[0m'
+
+#
+# How to use this script ?
+# ========================
+#   Allows to install any additional packages needed by IFAETopFramework to compile
+#   source install.sh <svn username>
+#
+
+echo $green"=> No additional package needed for IFAETopFramework package :-)"$normal
+
+return 0
+
