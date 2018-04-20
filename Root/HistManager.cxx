d35a4cfc68fbe568043c4c613d7e512364f8161a
tfarooque@ifae.es <tfarooque@at306.pic.es>
2018-04-20 04:58:04 +0200

    Clean commented code

--- Root/HistManager.cxx
+++ Root/HistManager.cxx
@@ -105,32 +105,7 @@ void HistManager::FinaliseTH1Bins(const string &s_hist){
     bool addOF = (hopt & FCHECK) ? (hopt & OFLOW) : m_addOF;
 
     HistManager::FinaliseTH1Bins(h1_it->second, addOF, addUF);
-    /*
-    int entries=h1_it->second->GetEntries(); // get number of entries of histogram to preserve it in the following
-    int nbins = h1_it->second->GetNbinsX();
-    if(addOF){
-      double v_of       = h1_it->second->GetBinContent(nbins+1);
-      double v_last     = h1_it->second->GetBinContent(nbins);
-      double e_of       = h1_it->second->GetBinError(nbins+1);
-      double e_last     = h1_it->second->GetBinError(nbins);
-      h1_it->second->SetBinContent(nbins, v_last + v_of);
-      h1_it->second->SetBinError(nbins, sqrt(e_last*e_last + e_of*e_of) );
-    }
-    if(addUF){
-      double v_uf       = h1_it->second->GetBinContent(0);
-      double v_first    = h1_it->second->GetBinContent(1);
-      double e_uf       = h1_it->second->GetBinError(0);
-      double e_first    = h1_it->second->GetBinError(1);
-      h1_it->second->SetBinContent(1, v_first + v_uf);
-      h1_it->second->SetBinError(1, sqrt(e_first*e_first + e_uf*e_uf) );
-    }
-    //Always set the underflow/overflow to zero, whether or not it is included in the first/last bin
-    h1_it->second->SetBinContent(0, 0.);
-    h1_it->second->SetBinError(0, 0.);
-    h1_it->second->SetBinContent(nbins+1, 0.);
-    h1_it->second->SetBinError(nbins+1, 0.);
-    h1_it->second->SetEntries(entries); // set the old number of entries for histogram so that it's not changed by the underflow/overflow adding procedure
-    */
+
     return;
 }
 
