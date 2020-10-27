0dbf8f05155d96ec59b28f2d4e2b972a6f551274
megglest <michael.eggleston@cern.ch>
2020-10-27 16:22:21 +0100

    Fixed SampleInfo function prototypes to reflect updated use case with accessing weight map

--- IFAETopFramework/SampleInfo.h
+++ IFAETopFramework/SampleInfo.h
@@ -22,11 +22,13 @@ public:
   void ReadSample( const std::string& dsID, const std::string& configFile);
     //_______________________________________
     //
-    inline double NWeightedEvents() const { return m_nWeightedEvents; }
-    inline double NWeightedEvents(const std::string& branch) const { return m_signalRWMap.at(branch); }
+    //inline double NWeightedEvents() const { return m_nWeightedEvents; }
+    //inline double NWeightedEvents(const std::string& branch) const { return m_signalRWMap.at(branch); }
+    double NWeightedEvents(const std::string& wgt_name) const;
     inline double CrossSection() const { return m_crossSection; }
-    inline double NormFactor( const double lumi = 1. ) { return ( m_ready ? (m_crossSection * lumi / m_nWeightedEvents) : 1. ); }
-    inline double NormFactor( const std::string& branch, const double lumi = 1. ) { return ( m_ready ? (m_crossSection * lumi / m_signalRWMap.at(branch)) : 1. ); }
+    //inline double NormFactor( const double lumi = 1. ) { return ( m_ready ? (m_crossSection * lumi / m_nWeightedEvents) : 1. ); }
+    //inline double NormFactor( const std::string& branch, const double lumi = 1. ) { return ( m_ready ? (m_crossSection * lumi / m_signalRWMap.at(branch)) : 1. ); }
+    double NormFactor(const std::string& wgt_name, const double lumi) const;
     inline const std::string& SampleName(){ return m_sampleName; }
     inline bool Ready() const { return m_ready; }
     inline std::map<std::string, double > SystWeightFactorMap() const { return m_systWeightFactorMap; }
